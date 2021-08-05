2559729fd9d097f23f4fd9228f3925a2a1b2b20f
tory <86025162+torylark@users.noreply.github.com>
2021-08-05 16:53:09 +0300

    Add files via upload

---
+++ homework19/script.js
@@ -0,0 +1,13 @@
+class Circle {
+   
+    constructor(radius) { 
+      this.radius = radius;
+    }
+    pi=3.14;
+    square = function(radius) {
+    return this.pi * radius**2;
+ }
+   static createDefault() {
+   }
+   //я не знаю как дальше ;D
+}
